6c388c92b3bdb49e8bf6c4359fad422d05e8b80a
hagi <revalies0@yahoo.co.jp>
2019-03-31 11:09:37 +0000

    kadaisaiteisyutumae

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :show, :update]
-
+  before_action :correct_user, only: [:destroy, :edit, :update, :show]
+  
   def index
     if logged_in?
       @task = current_user.tasks.build
